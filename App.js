8de307359e599dba3667ae492320c4da28c5224d
anggihnurh <cickozsiteid@gmail.com>
2018-08-01 13:32:51 +0700

    Statefull

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {
+        key: 'value'
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+expoort default App;
